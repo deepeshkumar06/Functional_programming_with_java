ce24457bdd7ee8649759f691ec9458c0de0d354c
deepeshkumar06 <deepeshkumar.p2004@gmail.com>
2025-05-31 20:43:50 +0530

    lambda function created

--- src/Programming/FP01Function.java
+++ src/Programming/FP01Function.java
@@ -6,16 +6,28 @@ public class FP01Function {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		printallno(List.of(12,3,4,5,6,2,3,9,7));
+		List<Integer> list = List.of(12,3,4,5,6,2,3,9,7);
+//		printallno(list);
+		printevenno(list);
 	}
 
-	public static void print(int num) {
-		System.out.println(num);
+//	public static void print(int num) {
+//		System.out.println(num);
+//	}
+	public static boolean isEven(int num) {
+		return num%2 == 0;
 	}
 	
-	private static void printallno(List<Integer> Numbers) {
+//	private static void printallno(List<Integer> Numbers) {
+//		// TODO Auto-generated method stub
+//		Numbers.stream().forEach(FP01Function :: print);
+//	}
+	private static void printevenno(List<Integer> Numbers) {
 		// TODO Auto-generated method stub
-		Numbers.stream().forEach(FP01Function :: print);
+		Numbers.stream().
+			//filter(FP01Function :: isEven). //method Reference
+			filter(num -> num%2 == 0).		//Lambda Function
+			forEach(System.out :: println); //Pre-defined Method Reference
 	}
 
 }
